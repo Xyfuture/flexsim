8e233d0f2b285e85e6190c4dfa6ed91fc55235ee
Xyfuture <138606237+wangxy-2000@users.noreply.github.com>
2024-01-01 22:21:42 +0800

    'add' op

---
+++ flexsim/marco_graph/marco_ops/add.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+from typing import Tuple
+
+from flexsim._node import Node
+from flexsim.marco_graph.marco_op import MarcoOp
+from flexsim._graph import Graph
+
+
---
+++ flexsim/marco_graph/marco_parser.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+
+
+class MarcoParser:
+    def __init__(self, network: nn.Module):
+        self.network = network
+
+    def make_marco_graph(self):
+        pass
+
+    def tensor_shape_inference(self):
+        pass
+
+    # 替换功能
