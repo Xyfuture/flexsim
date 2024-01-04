648d44c8c48120fd1044bb4ad48e569428392740
Xyfuture <138606237+wangxy-2000@users.noreply.github.com>
2024-01-04 22:09:52 +0800

    format

--- flexsim/hardware_component.py
+++ flexsim/hardware_component.py
@@ -8,14 +8,17 @@ class HardwareComponent:
     id_counter: int = 0
     id_map: Dict[int, HardwareComponent] = {}
 
-    def __init__(self):
+    def __init__(self, name: str):
         HardwareComponent.id_counter += 1
         self.hardware_id = HardwareComponent.id_counter
         HardwareComponent.id_map[self.hardware_id] = self
 
+        self.name = name
+
     @classmethod
-    def get_component_by_id(cls, id):
-        return cls.id_map[id]
+    def get_component_by_id(cls, hw_id):
+        return cls.id_map[hw_id]
+
 
     def get_machine_op_finish_time(self, machine_op: MachineOp, data_ready_time: int) -> int:
         """
@@ -34,14 +37,15 @@ class InterconnectComponent(HardwareComponent):
     InterconnectComponent can only transfer data, can not execute any micro ops.
     """
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, name: str):
+        super().__init__(name)
 
     def register_compo(self, compo: GeneralComponent):
         pass
 
     def compute_transfer_latency(self):
         pass
+
 
 class GeneralComponent(HardwareComponent):
     """
@@ -50,9 +54,8 @@ class GeneralComponent(HardwareComponent):
     """
 
     def __init__(self, name: str, parent_compo: Optional[GeneralComponent, None] = None, ):
-        super().__init__()
+        super().__init__(name)
 
-        self.name = name
         self.parent_compo = parent_compo
 
         self.outer_comm_compo: Optional[InterconnectComponent] = None
